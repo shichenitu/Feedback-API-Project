bfa720a74a6033feaeb68aa428792a2e8d915c57
shichenitu <shichenitu@163.com>
2026-01-19 19:06:42 +0100

    feat: redesign entities with multi-dimensional feedback and products

--- src/user/user.entity.ts
+++ src/user/user.entity.ts
@@ -1,18 +1,18 @@
-// enum: a list which can only be selected 
+// enum: a list which can be selected
 // Define the enum types 
 export enum UserRole{
     USER = 'user',
     ADMIN = 'admin',
 }
 
-// Define the Class of user
+// Define the Class of User
 export class User{
     id: string;
     name: string;
     email: string;
     passwordHash: string;
     role: UserRole; // it is from the enum definition above
-
+    phoneNumber: string;
     createAt: Date;
     updateAt: Date;
 }
